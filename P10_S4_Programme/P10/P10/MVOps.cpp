cfcc9b4381d262d56f1058f16afa9f9b4d452026
Maxime Lussier <maximelussier@gmail.com>
2011-08-11 11:50:32 -0400

    commented MVOps.cpp

--- P10_S4_Programme/P10/P10/MVOps.cpp
+++ P10_S4_Programme/P10/P10/MVOps.cpp
@@ -17,7 +17,12 @@ Ensemble de fonctions statiques afin d'effectuer des opérations sur des vecteur 
 Auteur: Maxime Lussier
 */
 
-// trouve le minimum et son indice. (checked working)
+//------------------------------------------
+//	Fonction pour trouver le minimum d'un vecteur et son indice.
+//	Entrées: le vecteur, sa longueur, un pointeur pour sortir l'index
+//	Retourne: la valeur du minimum
+//	Auteur: Maxime Lussier
+//------------------------------------------
 double MVOps::minimum(double* vector, int length, int* indice)
 {
 	double Ymin = vector[0];       // start with min = first element
@@ -33,7 +38,12 @@ double MVOps::minimum(double* vector, int length, int* indice)
 	return Ymin;
 }
 
-// trouve le maximum et son indice. (checked working)
+//------------------------------------------
+//	Fonction pour trouver le maximum d'un vecteur et son indice.
+//	Entrées: le vecteur, sa longueur, un pointeur pour sortir l'index
+//	Retourne: la valeur du maximum
+//	Auteur: Maxime Lussier
+//------------------------------------------
 double MVOps::maximum(double* vector, int length, int* indice)
 {
 	double Ymax = vector[0];       // start with max = first element
@@ -49,6 +59,12 @@ double MVOps::maximum(double* vector, int length, int* indice)
 	return Ymax;
 }
 
+//------------------------------------------
+//	Fonction pour trouver le maximum d'une matrice et son indice.
+//	Entrées: la matrice, le nombre de lignes, de colonnes, un pointeur vers un array pour sortir l'index
+//	Retourne: la valeur du maximum
+//	Auteur: Maxime Lussier
+//------------------------------------------
 double MVOps::maximum(double** X, int M, int N, int* indice)
 {
 	double Ymax = 0; 
@@ -65,7 +81,12 @@ double MVOps::maximum(double** X, int M, int N, int* indice)
 	return Ymax;
 }
 
-// Obtains a sub vector from index to index for doubles(Checked Working)
+//------------------------------------------
+//	Fonction pour obtenir un sous vecteur de doubles
+//	Entrées: le vecteur, l'index de départ, de fin
+//	Retourne: le sous vecteur de doubles
+//	Auteur: Maxime Lussier
+//------------------------------------------
 double* MVOps::subVector(double* original, int startIndex, int endIndex){
 
 	double* subV = new double[endIndex-startIndex+1];
@@ -76,7 +97,12 @@ double* MVOps::subVector(double* original, int startIndex, int endIndex){
 	return subV;
 }
 
-// Obtains a sub vector from index to index for ints (Checked Working)
+//------------------------------------------
+//	Fonction pour obtenir un sous vecteur d'entiers
+//	Entrées: le vecteur, l'index de départ, de fin
+//	Retourne: le sous vecteur d'entiers
+//	Auteur: Maxime Lussier
+//------------------------------------------
 int* MVOps::subVector(int* original, int startIndex, int endIndex){
 
 	int* subV = new int[endIndex-startIndex+1];
@@ -87,7 +113,13 @@ int* MVOps::subVector(int* original, int startIndex, int endIndex){
 	return subV;
 }
 
-// Obtain a sub matrix trom row to row and col to col (Checked Working)
+//------------------------------------------
+//	Fonction pour obtenir une sous-matrice de doubles
+//	Entrées: la matrice, la ligne minimum, la ligne finale,
+//           la colonne minimum, la colonne finale
+//	Retourne: la sous-matrice de doubles
+//	Auteur: Maxime Lussier
+//------------------------------------------
 double** MVOps::subMatrix(double** X, int rowI, int rowF, int colI, int colF){
 
 	double** Y = newMatrix(rowF-rowI+1, colF-colI+1);
@@ -99,7 +131,13 @@ double** MVOps::subMatrix(double** X, int rowI, int rowF, int colI, int colF){
 	return Y;
 }
 
-// trouve les indices des points supérieurs à evaluate (Checked Working)
+//------------------------------------------
+//	Fonction pour trouver les indices des points supérieurs à evaluate (Checked Working)
+//	Entrées: le vecteur, la valeur a evaluer, un pointeur vers le nombre de points trouves
+//	Retourne: le vecteur des indices
+//	Auteur: Matlab : Marc-Antoine Veilleux
+//             C++ : Maxime Lussier
+//------------------------------------------
 int* MVOps::FindPoints(double* Vecteur, double evaluate, int length, int* found){
 
 	int* temp = new int[length];
@@ -116,7 +154,14 @@ int* MVOps::FindPoints(double* Vecteur, double evaluate, int length, int* found)
 	return foundPoints;
 }
 
-// get values from vector at indexes in given vector... (Checked Working)
+
+//------------------------------------------
+//	Fonction pour trouver les valeurs d'un vecteur aux indices donnés (Checked Working)
+//	Entrées: le vecteur de doubles contenant les valeurs, le vecteur des indices,
+//  le nombre d'indices
+//	Retourne: le vecteur des valeurs
+//	Auteur: Maxime Lussier
+//------------------------------------------
 double* MVOps::getValuesAt(double* Values, int* index, int indexLength){
 
 	double* subV = new double[indexLength];
@@ -127,7 +172,12 @@ double* MVOps::getValuesAt(double* Values, int* index, int indexLength){
 	return subV;
 }
 
-// génère un vecteur de zéros de longeur spécifié (Checked Working)
+//------------------------------------------
+//	Fonction qui génère un vecteur zéros de longeur spécifiée (entiers) (Checked Working)
+//	Entrées: la longueur voulue
+//	Retourne: le vecteur des zéros
+//	Auteur: Maxime Lussier
+//------------------------------------------
 int* MVOps::zeros(int size){
 	int* zero = new int[size];
 	for(int i = 0; i < size;i++){
@@ -136,7 +186,12 @@ int* MVOps::zeros(int size){
 	return zero;
 }
 
-// génère une matrice de zéros de longeur spécifié (Checked Working)
+//------------------------------------------
+// 	génère une matrice de zéros de grandeur spécifiée (Checked Working)
+//	Entrées: le nombre de lignes, le nombre de colonnes
+//	Retourne: le vecteur des zéros
+//	Auteur: Maxime Lussier
+//------------------------------------------
 double** MVOps::zeros(int sizeL, int sizeC){
 	double** Z = new double*[sizeL];
 	for(int i = 0; i < sizeL; i++){
@@ -147,7 +202,12 @@ double** MVOps::zeros(int sizeL, int sizeC){
 	return Z;
 }
 
-// génère un vecteur de zéros de longeur spécifié (Checked Working)
+//------------------------------------------
+//	Fonction qui génère un vecteur zéros de longeur spécifiée (double) (Checked Working)
+//	Entrées: la longueur voulue
+//	Retourne: le vecteur des zéros
+//	Auteur: Maxime Lussier
+//------------------------------------------
 double* MVOps::zerosD(int size){
 	double* zero =  new double[size];
 	for(int i = 0; i < size;i++){
@@ -156,6 +216,12 @@ double* MVOps::zerosD(int size){
 	return zero;
 }
 
+//------------------------------------------
+//	Fonction qui retourne la colonne d'une matrice (Checked Working)
+//	Entrées: la matrice, la colonne voulue, le nombre de lignes
+//	Retourne: le vecteur colonne
+//	Auteur: Maxime Lussier
+//------------------------------------------
 double* MVOps::getColumn(double** In, int col, int nbRow)
 {
 	double* out = new double[nbRow];
@@ -165,6 +231,12 @@ double* MVOps::getColumn(double** In, int col, int nbRow)
 	return out;
 }
 
+//------------------------------------------
+//	Fonction qui retourne une matrice à partir d'un vecteur (Checked Working)
+//	Entrées: le vecteur, sa longueur
+//	Retourne: la matrice contenant le vecteur
+//	Auteur: Maxime Lussier
+//------------------------------------------
 double** MVOps::vectorToMatrix(double* vct, int length){
 	double** Y = new double*[length];
 	for(int i = 0; i < length; i++){
@@ -174,6 +246,12 @@ double** MVOps::vectorToMatrix(double* vct, int length){
 	return Y;
 }
 
+//------------------------------------------
+//	Fonction qui concatène des vecteurs (Checked Working)
+//	Entrées: le vecteur 1, sa longueur, le vecteur 2, sa longueur
+//	Retourne: le vecteur concaténé
+//	Auteur: Maxime Lussier
+//------------------------------------------
 double* MVOps::appendVectors(double* vect1, int length1, double* vect2, int length2){
 	double* Y = new double[length1+length2];
 	for(int i = 0; i < length1; i++){
@@ -185,6 +263,12 @@ double* MVOps::appendVectors(double* vect1, int length1, double* vect2, int leng
 	return Y;
 }
 
+//------------------------------------------
+//	Fonction qui fait la puissance d'un vecteur (Checked Working)
+//	Entrées: le vecteur, sa longueur, l'exposant
+//	Retourne: le vecteur calculé
+//	Auteur: Maxime Lussier
+//------------------------------------------
 double* MVOps::vectorPow(double* vector, int length, int exponent){
 	double* Y = new double[length];
 	for(int i = 0; i < length; i++){
@@ -193,6 +277,12 @@ double* MVOps::vectorPow(double* vector, int length, int exponent){
 	return Y;
 }
 
+//------------------------------------------
+//	Fonction qui additionne deux vecteurs (Checked Working)
+//	Entrées: le vecteur 1, le vecteur 2, leurs longueurs
+//	Retourne: le vecteur calculé
+//	Auteur: Maxime Lussier
+//------------------------------------------
 double* MVOps::vectorAdd(double* vector1, double* vector2, int length){
 	double* Y = new double[length];
 	for(int i = 0; i < length; i++){
@@ -201,6 +291,12 @@ double* MVOps::vectorAdd(double* vector1, double* vector2, int length){
 	return Y;
 }
 
+//------------------------------------------
+//	Fonction qui calcule le produit point par point d'un vecteur (Checked Working)
+//	Entrées: le vecteur 1, le vecteur 2, leurs longueurs
+//	Retourne: le vecteur calculé
+//	Auteur: Maxime Lussier
+//------------------------------------------
 double* MVOps::vectorDotProduct(double* vector1, double* vector2, int length){
 	double* Y = new double[length];
 	for(int i = 0; i < length; i++){
@@ -209,6 +305,12 @@ double* MVOps::vectorDotProduct(double* vector1, double* vector2, int length){
 	return Y;
 }
 
+//------------------------------------------
+//	Fonction qui calcule le produit d'un scalaire (double) par un vecteur (Checked Working)
+//	Entrées: le vecteur, le scalaire, la longueur du vecteur
+//	Retourne: le vecteur calculé
+//	Auteur: Maxime Lussier
+//------------------------------------------
 double* MVOps::vectorProduct(double* vector1, double doub, int length){
 	double* Y = new double[length];
 	for(int i = 0; i < length; i++){
@@ -217,6 +319,12 @@ double* MVOps::vectorProduct(double* vector1, double doub, int length){
 	return Y;
 }
 
+//------------------------------------------
+//	Fonction qui calcule le produit d'un scalaire (entier) par un vecteur (Checked Working)
+//	Entrées: le vecteur, le scalaire, la longueur du vecteur
+//	Retourne: le vecteur calculé
+//	Auteur: Maxime Lussier
+//------------------------------------------
 double* MVOps::vectorProduct(double* vector1, int in, int length){
 	double* Y = new double[length];
 	for(int i = 0; i < length; i++){
@@ -225,8 +333,12 @@ double* MVOps::vectorProduct(double* vector1, int in, int length){
 	return Y;
 }
 
-
-// Inverse d'une matrice
+//------------------------------------------
+//	Fonction qui calcule l'inverse d'une matrice (Checked Working)
+//	Entrées: la matrice, son ordre (N = M)
+//	Retourne: la matrice inverse
+//	Auteur: Maxime Lussier
+//------------------------------------------
 double** MVOps::matrixInversion(double** X, int ordre)
 {
 	double** Y = newMatrix(ordre, ordre);
@@ -253,7 +365,13 @@ double** MVOps::matrixInversion(double** X, int ordre)
 	return Y;
 }
 
-// Calculer la mineure des éléments (row,col)
+//------------------------------------------
+//	Fonction pour Calculer la mineure des éléments (row, col) (Checked Working)
+//	Entrées: la matrice d'entrée, la matrice de sortie,
+//  la ligne et la colonne de la mineure à calciler, son ordre (N = M)
+//	Retourne: rien (en référence)
+//	Auteur: Maxime Lussier
+//------------------------------------------
 void MVOps::mineure(double** X, double** Y, int row, int col, int ordre)
 {
 	// indique quelle colonne et ligne est copiée
@@ -274,7 +392,12 @@ void MVOps::mineure(double** X, double** Y, int row, int col, int ordre)
 	}
 }
 
-// Calculer le determinant récursivement.
+//------------------------------------------
+//	Fonction pour calculer le déterminant (récursivement) (Checked Working)
+//	Entrées: la matrice d'entrée, l'ordre (M=N)
+//	Retourne: le déterminant
+//	Auteur: Maxime Lussier
+//------------------------------------------
 double MVOps::determinant(double **X, int ordre)
 {
 	// arrèter quand l'ordre est de 1
@@ -301,7 +424,13 @@ double MVOps::determinant(double **X, int ordre)
 	return det;
 }
 
-// Calculer le produit matriciel
+//------------------------------------------
+//	Fonction pour calculer le produit matriciel (Checked Working)
+//	Entrées: la matrice 1, son nombre de lignes, de colonnes,
+//   		 la matrice 2, son nombre de lignes, de colonnes.
+//	Retourne: la matrice calculée
+//	Auteur: Maxime Lussier
+//------------------------------------------
 double** MVOps::matrixProduct(double** X1, int M1, int N1, double** X2, int M2, int N2){
 	double** Y = newMatrix(M1,N2);
 	if(N1!=M2)
@@ -319,7 +448,14 @@ double** MVOps::matrixProduct(double** X1, int M1, int N1, double** X2, int M2, 
 	return Y;
 }
 
-// Calculer le produit matriciel (avec un vecteur)
+
+//------------------------------------------
+//	Fonction pour calculer le produit matriciel avec un vecteur (Checked Working)
+//	Entrées: la matrice 1, son nombre de lignes, de colonnes,
+//   		 le vecteur  , son nombre de lignes.
+//	Retourne: la matrice calculée
+//	Auteur: Maxime Lussier
+//------------------------------------------
 double** MVOps::matrixProduct(double** X1, int M1, int N1, double* X2, int M2){
 	double** Y = newMatrix(M1, 1);
 	if(N1!=M2)
@@ -335,12 +471,25 @@ double** MVOps::matrixProduct(double** X1, int M1, int N1, double* X2, int M2){
 	return Y;
 }
 
+
+//------------------------------------------
+//	Fonction pour supprimer une matrice de la mémoire (Checked Working)
+//	Entrées: la matrice, son nombre de lignes.
+//	Retourne: rien.
+//	Auteur: Maxime Lussier
+//------------------------------------------
 void MVOps::deleteMatrix(double** X, int M){
 	for(int i = 0; i < M; i++)	// C'est comme ca qu'on efface une matrice.
 		delete [] X[i];			//
 	delete [] X;				//
 }
 
+//------------------------------------------
+//	Fonction pour allouer une matrice d'ordre MxN (Checked Working)
+//	Entrées: le nombre de lignes, de colonnes.
+//	Retourne: la matrice créée.
+//	Auteur: Maxime Lussier
+//------------------------------------------
 double** MVOps::newMatrix(int M, int N){
 	double** A = new double*[M];
 	for(int i = 0; i < M; i++){
@@ -349,6 +498,12 @@ double** MVOps::newMatrix(int M, int N){
 	return A;
 }
 
+//------------------------------------------
+//	Fonction pour calculer la moyenne d'une matrice (Checked Working)
+//	Entrées: la matrice, le nombre de lignes, de colonnes.
+//	Retourne: la moyenne.
+//	Auteur: Maxime Lussier
+//------------------------------------------
 double MVOps::mean(double** X, int M, int N){
 	double temp = 0;
 	for(int i = 0; i < M; i++){
@@ -359,6 +514,12 @@ double MVOps::mean(double** X, int M, int N){
 	return temp/((double)M*(double)N);
 }
 
+//------------------------------------------
+//	Fonction pour calculer la moyenne d'une sous-matrice (Checked Working)
+//	Entrées: la matrice, la ligne initiale, finale, la colonne initiale, finale.
+//	Retourne: la moyenne.
+//	Auteur: Maxime Lussier
+//------------------------------------------
 double MVOps::mean(double** X, int M1, int M2, int N1, int N2){
 	double temp = 0;
 	for(int i = M1; i < M2; i++){
@@ -369,10 +530,22 @@ double MVOps::mean(double** X, int M1, int M2, int N1, int N2){
 	return temp/((double)(M2-M1)*(double)(N2-N1));
 }
 
+//------------------------------------------
+//	Fonction pour calculer pythagore (Checked Working)
+//	Entrées: la valeur 1, la valeur 2
+//	Retourne: le résultat calculé.
+//	Auteur: Maxime Lussier
+//------------------------------------------
 double MVOps::pythagore(double A, double B){
 	return sqrt(pow(A, 2)+pow(B,2));
 }
 
+//------------------------------------------
+//	Fonction pour garder les trois point les plus perpendiculaires
+//	Entrées: matrice des points, nombre à filtrer
+//	Retourne: matrice des trois points
+//	Auteur: Maxime Lussier
+//------------------------------------------
 double** MVOps::garderPerpendiculaires(double** points, int Nb){
 	double** bons = MVOps::newMatrix(3, 2);
 	double A[2] = {0, 0};
